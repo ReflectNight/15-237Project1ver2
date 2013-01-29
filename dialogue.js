4748a20b56a20f72ef7b10e8e7f6bdc293bf4c2f
Angela Qiu <aqiu@andrew.cmu.edu>
2013-01-29 00:24:39 -0500

    MULTILINE WORKS YATTA

--- dialogue.js
+++ dialogue.js
@@ -15,17 +15,19 @@ function drawDialogue(){
 
 //prints whatever it tells you to, can make it multiline
 function printDialogue(txt){
-	//var multiline= txt.split(';');
+
 	var image = drawDialogue();
-	image.onload = function(){ctx.drawImage(image, 0, 450);
-	ctx.textAlign = 'left';
-	ctx.font="15pt Helvetica";
-	ctx.fillStyle = "black";
-	ctx.fillText(txt,30,500);
+	image.onload = function(){
+		ctx.drawImage(image, 0, 450);
+		ctx.textAlign = 'left';
+		ctx.font="15pt Helvetica";
+		ctx.fillStyle = "black";
+		//ctx.fillText(txt,30,500);
+		var multiline= txt.split(';');//tokenizer
+		var line = 500;
+		for (var i=0; i<multiline.length; i++){
+		ctx.fillText(multiline[i],30,line+(i*30));
 	}
-	/*
-	var line = 500;
-	for( var i=0; i<multiline.length; i++{
-		ctx.fillText(txt,30,line+(i*30)); */
-		
+	}
+	
 }
