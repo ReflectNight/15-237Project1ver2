b2f3a03c255825ad7afcf16fdd466d2e9b295fc5
ReflectNight <leftsidespire@gmail.com>
2013-01-28 23:36:03 -0500

    Update endscene.js

--- endscene.js
+++ endscene.js
@@ -4,19 +4,69 @@
 
 // getEndStats(): Returns a string of achievements.
 function getEndStats(){
+	var achievements = [""];
+	
+	//number of friends
+	var numFriends = 2;
+	for(var i = 0; i < NPC.length; i++){
+		if (NPC[i].done)
+			numFriends++;
+	}
+	
+	if (numFriends === 2)
+		achievements.push("We're your only friends. That's sad.");
+	else achievements.push("You have " + numFriends + "! Good for you.");
+	
+	//int
+	//str
+	//kosbie
+	
+	return achievements;
+}
+
+// drawGameEnd(): Draws end game screen. 
+function drawGameEnd(){
+	console.log("drawGameEnd");
+
+	ctx.fillStyle = "rgba(100, 100, 100, 0.5)";
+	ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+	
+	ctx.fillStyle = "white";
+	ctx.font = 'bold 40pt Arial';
+	ctx.textAlign = "center";
+	ctx.fillText("Game Over", canvasCenter, 140);
+	ctx.fillStyle = "black";
+	ctx.strokeText("Game Over", canvasCenter, 140);
+	
+	ctx.fillStyle = "white";
+	ctx.font = 'bold 20pt Arial';
+	ctx.fillText("Press space to continue", canvasCenter, 300);
+	ctx.fillStyle = "black";
+	ctx.strokeText("Press space to continue", canvasCenter, 300);
+	
+	screen = 3;
 }
 
 // drawEndPage(): Draws the end scene with all of your achievements.
 function drawEndPage(){
-	ctx.fillStyle = "rgba(230, 170, 200, 0.25)";
-	ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+	console.log("drawEndPage");
+	//drawEndPage
+	
+	var endImage = new Image();
+	endImage.src = "Images/Other/results.png";
+	endImage.onload = function(){ctx.drawImage(endImage, 0, 0)
+	
+	var achievements = getEndStats();
 	
 	ctx.fillStyle = "black";
-	ctx.font = 'bold 40pt Arial';
-	ctx.textAlign = "center";
-	ctx.strokeText("Game Over", canvasCenter, 140);
-	
 	ctx.font = 'bold 20pt Arial';
-	ctx.strokeText("Press space to continue", canvasCenter, 300);
-	
+	ctx.textAlign = "left";
+	for(var i = 0; i < achievements.length; i++){
+		ctx.fillText("HI", 0, 0);
+		ctx.fillText(achievements[i], 50, i*25 + 100);
+		console.log(achievements[i]);
+	}
+
+	};
+	screen = 4;
 }
