9540a21ea25dafdac8efef44c4652d469608c5a5
Sarah Chen <swchen@andrew.cmu.edu>
2013-01-29 00:02:47 -0500

    dialogue fix

--- dialogue.js
+++ dialogue.js
@@ -9,19 +9,20 @@ function drawDialogue(){
 	//ctx.fillRect(0,450,600,150);
 	var image = new Image();
 	image.src = "Images/Worlds/dialogue.png";
-	ctx.drawImage(image, 0, 450);
+	
+	return image;
 }
 
 //prints whatever it tells you to, can make it multiline
 function printDialogue(txt){
-	
 	//var multiline= txt.split(';');
-	drawDialogue();
+	var image = drawDialogue();
+	image.onload = function(){ctx.drawImage(image, 0, 450);
 	ctx.textAlign = 'left';
 	ctx.font="15pt Helvetica";
 	ctx.fillStyle = "black";
 	ctx.fillText(txt,30,500);
-	
+	}
 	/*
 	var line = 500;
 	for( var i=0; i<multiline.length; i++{
